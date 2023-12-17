262100a6a290e3f8a2d9fd4f4b2ccaf12c25bae0
Galpein <galigalanare@gmail.com>
2023-12-17 21:53:35 +0100

    updated proxy and built static files

--- server/server.js
+++ server/server.js
@@ -245,7 +245,7 @@ app.get('/api/vitalSigns', async (req, res) => {
 app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-});
+})
 
 app.listen(5000, () => { 
     console.log("Server started on port 5000");
